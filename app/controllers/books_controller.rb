c4b63af428644e45c347cc3f6ff71bd9d1a0f618
Jonathan Queiroz <jonathanqueiroz94@gmail.com>
2022-03-25 22:00:37 -0300

    Remove unused jbuilder and adjustments on books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,36 +20,25 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
 
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to book_url(@book), notice: 'Book was successfully created.' }
-        format.json { render :show, status: :created, location: @book }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    if @book.save
+      redirect_to book_url(@book), notice: 'Livro foi criado com sucesso.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to book_url(@book), notice: 'Book was successfully updated.' }
-        format.json { render :show, status: :ok, location: @book }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    if @book.update(book_params)
+      redirect_to book_url(@book), notice: 'Livro foi atualizado com sucesso.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @book.destroy
 
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to books_url, notice: 'O livro foi apagado com sucesso.'
   end
 
   def search
