250e1e2fbdf46da32ac0fa44ef4a66828c0db910
Jonathan Queiroz <jonathanqueiroz94@gmail.com>
2022-03-25 22:02:00 -0300

    Added validations on books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,13 @@
 class Book < ApplicationRecord
+  require 'uri'
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :cover_url, format: { with: URI.regexp }, if: :cover_url_presence?
+  validates :author_name, presence: true
+
+  def cover_url_presence?
+    cover_url.present?
+  end
+
 end
