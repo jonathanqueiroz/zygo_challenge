cf3e24b7985dcc0041d20355f1785ad0589f971d
Jonathan Queiroz <jonathanqueiroz94@gmail.com>
2022-03-27 22:51:15 -0300

    added tests to model book

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with all arguments' do
+    book = FactoryBot.create(:book)
+    expect(book).to be_valid
+  end
+
+  it 'is invalid with nil title' do
+    book = FactoryBot.build(:book, title: nil)
+    expect(book).to be_invalid
+  end
+
+  it 'is invalid with nil description' do
+    book = FactoryBot.build(:book, description: nil)
+    expect(book).to be_invalid
+  end
+
+  it 'is invalid with cover_url not being a valid url' do
+    book = FactoryBot.build(:book, cover_url: 'teste123')
+    expect(book).to be_invalid
+  end
+
+  it 'is invalid with nil author_name' do
+    book = FactoryBot.build(:book, author_name: nil)
+    expect(book).to be_invalid
+  end
 end
